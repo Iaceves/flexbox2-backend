41e368d128d0018ccf6d1be38a7f8b6f23dd8021
Ismael Aceves <ismael@Ismaels-MBP.lan1>
2024-02-13 16:23:57 -0500

    fix flexBoxContactSeed files

--- seed/flexboxContactSeed.js
+++ seed/flexboxContactSeed.js
@@ -1,24 +1,24 @@
 const db = require("../db");
-const { ContactEmailsSchema } = require("../models/contactEmails");
+const { ContactEmail } = require("../models/index");
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const emails = async () => {
-  const Email1 = await new ContactEmailsSchema({
+  const Email1 = await new ContactEmail({
     email: "Juan3@gmail.com",
-    text: "Can your company resolve coder problems?",
+    contactQuestions: "Can your company resolve coder problems?",
   });
   Email1.save();
 
-  const Email2 = await new ContactEmailsSchema({
+  const Email2 = await new ContactEmail({
     email: "mark1@gmail.com",
-    text: "How fast can your build a working website?",
+    contactQuestions: "How fast can your build a working website?",
   });
   Email2.save();
 
-  const Email3 = await new ContactEmailsSchema({
+  const Email3 = await new ContactEmail({
     email: "luisa6@gmail.com",
-    text: "Can you create a website for me in the next 6 months?",
+    contactQuestions: "Can you create a website for me in the next 6 months?",
   });
   Email3.save();
 };
